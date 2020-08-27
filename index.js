456c2d43638910433099c0a92a3f7953524f94bd
Robrecht Cannoodt <rcannood@gmail.com>
2020-08-27 08:44:21 +0200

    detect a new type of buffer

--- index.js
+++ index.js
@@ -151,52 +151,51 @@ function savetomapjson(savefile) {
   let mindex = mapstartindex + 16;
 
   for (let i = 0; i < tiles; i++) {
-    const num2 = bin.readUInt8(mindex + 49);
-    const num = bin.readUInt32LE(mindex + 51);
+    const num1 = bin.readUInt8(mindex + 49);
+    const num2 = bin.readUInt8(mindex + 51);
     let buflength = 0;
 
-    num && (buflength += 24);
-    (num2 >= 64) && (buflength += 17);
+    (num1 & 64) && (buflength += 17);
+    (num2 & 1) && (buflength += 24);
+    (num2 & 2) && (buflength += 44);
 
     // See bin-structure.md for WIP documentation on what each of these values are
     map.tiles.push({
       'x': i % mapsizedata[tiles].x,
       'y': Math.floor(i / mapsizedata[tiles].x),
-      'hex-location': mindex,
-      'tile-length': 55 + buflength,
-      'int16-1': bin.readUInt16LE(mindex),
-      'int16-2': bin.readUInt16LE(mindex + 2),
-      'int16-3': bin.readUInt16LE(mindex + 4),
-      'int16-4': bin.readUInt16LE(mindex + 6),
-      'landmass index?': bin.readUInt16LE(mindex + 8),
-      'landmass index + 1?': bin.readUInt16LE(mindex + 10),
-      'Int8-1': bin.readUInt8(mindex + 12),
-      'Int8-2': bin.readUInt8(mindex + 13),
-      'Int8-3': bin.readUInt8(mindex + 14),
-      'Int8-4': bin.readUInt8(mindex + 15),
-      'Int8-5': bin.readUInt8(mindex + 16),
-      'Int8-6': bin.readUInt8(mindex + 17),
-      'Int8-7': bin.readUInt8(mindex + 18),
-      'Int8-8': bin.readUInt8(mindex + 19),
-      '?-1': bin.readInt16LE(mindex + 20),
-      'terrain type again?': bin.slice(mindex + 22, mindex + 26),
-      '?-2': bin.readInt8(mindex + 26),
-      'resource?': bin.slice(mindex + 27, mindex + 31),
-      'Resource boolean': bin.readInt16LE(mindex + 31),
-      'goody hut?': bin.slice(mindex + 33, mindex + 37),
-      'Int8-9': bin.readUInt8(mindex + 37),
-      'Road Level': bin.readUInt8(mindex + 38),
-      'Int8-b': bin.readUInt8(mindex + 39),
-      'Tile Appeal': bin.readInt16LE(mindex + 40),
-      'Int8-c': bin.readUInt8(mindex + 42),
-      'Int8-d': bin.readUInt8(mindex + 43),
-      'Int8-e': bin.readUInt8(mindex + 44),
-      'River border count': bin.readUInt8(mindex + 45),
-      'rivermap': bin.readUInt8(mindex + 46).toString(2).padStart(6, '0'),
-      'cliffmap': bin.readInt8(mindex + 47).toString(2).padStart(6, '0'),
-      '?-9': bin.slice(mindex + 48, mindex + 51),
-      'buffer length boolean': bin.readInt32LE(mindex + 51),
-      'buffer': bin.slice(mindex + 55, mindex + 55 + buflength),
+      'hex_location': mindex,
+      'tile_length': 55 + buflength,
+      'int16_1': bin.readUInt16LE(mindex),
+      'int16_2': bin.readUInt16LE(mindex+2),
+      'int16_3': bin.readUInt16LE(mindex+4),
+      'int16_4': bin.readUInt16LE(mindex+8),
+      'landmass': bin.readUInt32LE(mindex+8),
+      'terrain': bin.readUInt32LE(mindex+12),              // e.g. snow, tundra, hills, mountains
+      'feature': bin.readUInt32LE(mindex+16),              // e.g. forest
+      'natural_wonder': bin.readInt16LE(mindex + 20),
+      'continent': bin.readUInt32LE(mindex + 22),
+      'number_of_units': bin.readInt8(mindex + 26),
+      'resource_type': bin.readUInt32LE(mindex + 27),
+      'resource_boolean': bin.readInt16LE(mindex + 31),
+      'improvement': bin.readUInt32LE(mindex + 33),
+      'improvement_player': bin.readUInt8(mindex + 37),
+      'road_level': bin.readUInt8(mindex + 38),            // 1: classical, 2: industrial, 3: modern
+      'road_level_2': bin.readUInt8(mindex + 39),          // why a second time?
+      'appeal': bin.readInt16LE(mindex + 40),
+      'river_e': bin.readUInt8(mindex + 42),               // don't know what different values stand for
+      'river_se': bin.readUInt8(mindex + 43),              // don't know what different values stand for
+      'river_sw': bin.readUInt8(mindex + 44),              // don't know what different values stand for
+      'river_count': bin.readUInt8(mindex + 45),
+      'river_map': bin.readUInt8(mindex + 46),             // river 6 bits: NW, W, SW, SE, E, NE
+      'cliff_map': bin.readInt8(mindex + 47),              // cliff 6 bits: NW, W, SW, SE, E, NE
+      'flags1': bin.readUInt8(mindex + 48),
+      'flags2': bin.readUInt8(mindex + 49),
+      'flags3': bin.readUInt8(mindex + 50),
+      'flags4': bin.readUInt8(mindex + 51),
+      'flags5': bin.readUInt8(mindex + 52),                // empty?
+      'flags6': bin.readUInt8(mindex + 53),                // empty?
+      'flags7': bin.readUInt8(mindex + 54),                // empty?
+      'buffer': bin.slice(mindex + 55, mindex + 55 + buflength).toString('hex'),
     });
 
     mindex += 55 + buflength;
